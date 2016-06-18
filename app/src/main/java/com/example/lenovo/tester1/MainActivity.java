249226f6eeb0875c179461a2a5081fe73ad8892e
ankit96 <ankit.sagwekar@yahoo.co.in>
2016-06-18 15:40:48 +0530

    added placepicker

--- app/src/main/java/com/example/lenovo/tester1/MainActivity.java
+++ app/src/main/java/com/example/lenovo/tester1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lenovo.tester1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,15 +11,24 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 Button b;
+    private TextView get_place;
     Firebase mRef;
+    EditText t2;
+    Button placebutton;
+    int PLACE_PICKER_REQUEST=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +40,12 @@ Button b;
         b=(Button)findViewById(R.id.button);
         EditText t=(EditText)findViewById(R.id.editText);
         EditText t1=(EditText)findViewById(R.id.editText2);
-        EditText t2=(EditText)findViewById(R.id.editText3);
+         t2=(EditText)findViewById(R.id.editText3);
+        get_place=(TextView)findViewById(R.id.nameaddress);
 
         String name=t.getText().toString();
-        String pin=t1.getText().toString();
-        String contact=t2.getText().toString();
+        String pin=t2.getText().toString();
+        String contact=t1.getText().toString();
 
         /*RadioGroup rg1=(RadioGroup)findViewById(R.id.radioGroup);
         int selectedId = rg1.getCheckedRadioButtonId();
@@ -63,10 +74,35 @@ Button b;
 
 
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(this, data);
+                String address=String.format("address: %s",place.getAddress());
+                String name = String.format("name: %s", place.getName());
+                Toast.makeText(this, name+address, Toast.LENGTH_LONG).show();
+               // get_place.setText(name);
+                //t2.append("400037");
+            }
+        }
+    }
+
     public void fun2(View v)
     {
         Intent in=new Intent(this,ViewList.class);
         this.startActivity(in);
+    }
+    public void print(View v)
+    {
+        try {
+            //Toast.makeText(this,"Hello", Toast.LENGTH_LONG).show();
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+            Intent i= builder.build(MainActivity.this);
+            startActivityForResult(i, PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesNotAvailableException | GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        }
     }
 
 
